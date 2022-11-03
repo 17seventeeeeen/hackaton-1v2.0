e550dae4c51f5a2ed452ee46579ef3706694b7cc
iskender <even10iq@gmail.com>
2022-11-03 18:41:24 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// var tablinks = document.getElementsByClassName("wrapper-right__skills");
+// var tabcontents = document.getElementsByClassName("wrapper-right__links");
+
+// function openskills(tabname){
+//     for(tablink of tablinks){
+//         tablink.classList.remove("active-link");
+//     }
+// }
+
+// function openskills(tabname){
+//     for(tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-skills");
+//     }
+//     Event.currentTarget.classList.add("active-link")
+// }
+
